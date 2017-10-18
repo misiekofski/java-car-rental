445dce51281738665ae206a9062ff595e14c22a0
misiekofski <dobrzycki@gmail.com>
2017-10-18 21:44:54 +0200

    Second version updated

--- src/RentalApp.java
+++ src/RentalApp.java
@@ -1,71 +1,86 @@
 import java.util.*;
 
-
 public class RentalApp {
-	static Collection<Car> carList = new HashSet<Car>();  // tu trzymamy liste aut
+	static Collection<Car> carList = new HashSet<Car>(); // tu trzymamy liste
+	// aut
 	final static String MAIN_MENU = "1. Dodaj samochód\n2. Usuń samochód\n3. Edytuj samochód\n4. Sprawdź samochód\n5. Wypożycz samochód\n6. Oddaj samochód\n0. Wyjdź z programu";
 	final static String CAR_NOT_FOUND = "Car was not found in database.";
-	static Scanner sc = new Scanner(System.in); 
-	
-	
-	public static void addCar (){  
-		System.out.println("Registration number: ");
-		String regNo=sc.nextLine();
+	static Scanner sc = new Scanner(System.in);
+
+	public static void addCar(String registration) {
+		String regNo = registration;
+		for (Car s : carList) {
+			if (regNo.equals(s.getRegNumber())) {
+				System.out.println("Car with this reg no exist in DB");
+				return;
+			}
+		}
 		System.out.println("Enter producer: ");
-		String producer=sc.nextLine();
+		String producer = sc.nextLine();
 		System.out.println("Enter model: ");
-		String model=sc.nextLine();
+		String model = sc.nextLine();
 
 		carList.add(new Car(regNo, producer, model));
+	}
 
-	}
-	
-	public static void deleteCar(){
-		System.out.println("Enter registration to delete: ");
-		String regNo = sc.nextLine();
-
-		for (Car s : carList) {
-			if (regNo.equals(s.getRegNumber())) {
-				carList.remove(s);
-			}
-		}
-	}
-	
-	public static void editCar(){
-		System.out.println("Enter registration to edit: ");
-		String regNo = sc.nextLine();
-		for (Car s : carList) {
-			if (regNo.equals(s.getRegNumber())) {
-				// wywalamy
-				carList.remove(s);
-				//i tu trzeba dodać edytowany samochód ponownie
-			}
-		}
-		
-	}
-	
-	public static void checkCar(){
-		System.out.println("Enter registration to rent: ");
-		String regNo = sc.nextLine();
+	public static void deleteCar(String registration) {
+		String regNo = registration;
 		boolean carNotFound = true;
-		
+
 		for (Car s : carList) {
 			if (regNo.equals(s.getRegNumber())) {
-					s.checkIsCarRented();
-					carNotFound = false;
-				}
+				carList.remove(s);
+				carNotFound = false;
 			}
-		
+		}
+
+		if (carNotFound) {
+			System.out.println(CAR_NOT_FOUND);
+		}
+	}
+
+	public static void editCar(String registration) {
+		String regNo = registration;
+		boolean carNotFound = true;
+
+		for (Car s : carList) {
+			if (regNo.equals(s.getRegNumber())) {
+				// wywalamy znaleziony samochód o znanej rejestracji
+				carList.remove(s);
+				// i tu trzeba dodać edytowany samochód ponownie
+				System.out.println("Enter producer: ");
+				String producer = sc.nextLine();
+				System.out.println("Enter model: ");
+				String model = sc.nextLine();
+				carList.add(new Car(regNo, producer, model));
+				carNotFound = false;
+			}
+		}
+
 		if (carNotFound) {
 			System.out.println(CAR_NOT_FOUND);
 		}
 
 	}
 
-	
-	public static void rentCar(){
-		String regNo = sc.nextLine();
-		System.out.println("Enter registration to rent: ");
+	public static void checkCar(String registration) {
+		boolean carNotFound = true;
+
+		for (Car s : carList) {
+			if (registration.equals(s.getRegNumber())) {
+				s.checkIsCarRented();
+				carNotFound = false;
+			}
+		}
+
+		if (carNotFound) {
+			System.out.println(CAR_NOT_FOUND);
+		}
+
+	}
+
+	public static void rentCarFromRental(String registration) {
+		String regNo = registration;
 		boolean carNotFound = true;
 
 		for (Car s : carList) {
@@ -74,64 +89,79 @@ public class RentalApp {
 				carNotFound = false;
 			}
 		}
-		
+
 		if (carNotFound) {
 			System.out.println(CAR_NOT_FOUND);
 		}
 
-		
 	}
-	
-	public static void returnCarToRental(){
-		
-		System.out.println("Enter registration to rent: ");
-		String regNo = sc.nextLine();
+
+	public static void returnCarToRental(String registration) {
+		String regNo = registration;
+		boolean carNotFound = true;
 
 		for (Car s : carList) {
 			if (regNo.equals(s.getRegNumber())) {
 				s.returnCar();
+				carNotFound = false;
 			}
 		}
-		
+
+		if (carNotFound) {
+			System.out.println(CAR_NOT_FOUND);
+		}
+
 	}
-	
-	
+
+	public static String askForRegistration() {
+		System.out.println("Enter registration for car you want to process: ");
+		String regNo = sc.nextLine();
+		return regNo;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(MAIN_MENU);
-		
+		String regNo;
+
 		boolean quit = false;
 		int menuItem;
 		do {
-			 System.out.print("Choose menu item: ");
-             menuItem = sc.nextInt();
-             sc.nextLine();
-             switch (menuItem) {
-             case 1:
-            	   addCar();
-                   break;
-             case 2:
-                   deleteCar();
-                   break;
-             case 3:
-                   editCar();
-                   break;
-             case 4:
-                   checkCar();
-            	   break;
-             case 5:
-                   rentCar();
-                   break;
-             case 6:
-                 returnCarToRental();
-                 break;
-             case 0:
-                   quit = true;
-                   break;
-             default:
-                   System.out.println("Option not available.");
-             }
-       } while (!quit);
-		
+			System.out.print("Choose menu item: ");
+			menuItem = sc.nextInt();
+			sc.nextLine();
+			switch (menuItem) {
+			case 1:
+				regNo = askForRegistration();
+				addCar(regNo);
+				break;
+			case 2:
+				regNo = askForRegistration();
+				deleteCar(regNo);
+				break;
+			case 3:
+				regNo = askForRegistration();
+				editCar(regNo);
+				break;
+			case 4:
+				regNo = askForRegistration();
+				checkCar(regNo);
+				break;
+			case 5:
+				regNo = askForRegistration();
+				rentCarFromRental(regNo);
+				break;
+			case 6:
+				regNo = askForRegistration();
+				returnCarToRental(regNo);
+				break;
+			case 0:
+				quit = true;
+				break;
+			default:
+				System.out.println("Option not available.");
+			}
+		} while (!quit);
+
 	}
 
 }
--- src/testCarList.java
+++ src/testCarList.java
@@ -1,18 +1,31 @@
 import static org.junit.Assert.*;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 import org.junit.Before;
 import org.junit.Test;
 
 
 public class testCarList {
+	Collection<Car> carList = new HashSet<Car>();
+	Car car1 = new Car("DDZ 4156", "Opel", "Kadet");
+	Car car2 = new Car("DWR 6661", "Ford", "Fiesta");
+	Car car3 = new Car("DW 4561", "Mitsubishi", "Lancer");
+	Car car4 = new Car("D1 1234", "Hyundai", "i40");
+	
 
 	@Before
-	public void setUp() throws Exception {
+	public void init() throws Exception {
+		carList.add(car1);
+		carList.add(car2);
+		carList.add(car3);
+		carList.add(car4);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRentAndReturnCar() {
+	    fail ("Dupa!");
 	}
 
 }
